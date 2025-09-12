913f713f81d25e88b202f58ad3c8350e180a35f4
Wesley Cesar <wesleycbs2000@outlook.com>
2025-09-12 09:21:15 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Consoles',
+  webDir: 'www'
+};
+
+export default config;
